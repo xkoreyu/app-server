543fdff5d09e163a68ce1e0f0476220279b3456c
yu <xkorey@dingtalk.com>
2018-04-25 14:35:29 +0800

    load config from config server
    user springcloud config server

--- src/main/java/com/app/data/server/ServerApplication.java
+++ src/main/java/com/app/data/server/ServerApplication.java
@@ -1,12 +1,15 @@
 package com.app.data.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
 @EnableEurekaServer
+@EnableAutoConfiguration
 @SpringBootApplication
+@Configuration
 public class ServerApplication {
 
 	public static void main(String[] args) {
